48de572b8a0ccf9d952a6024adf5796aecd5be5b
Sharath <sballari@sapient.com>
2018-01-16 11:21:21 +0530

    Layout Change

--- kc.js
+++ kc.js
@@ -16,16 +16,16 @@ function onLoadData(){
 		var param = params[1].split("&");
 		var name = param[1].split("=")[1];
 		$.get( "https://www.reddit.com/r/"+name+"/hot.json", function( data ) {
-	  		data.data.children[3].data.title
 	  		for(var j=1;j<data.data.children.length;j++){
 	  			if(j%2 == 1){
-	  			$('#redditnews').append('<div class="row "><div class="col-lg-12 bg-light " ><a href="https://reddit.com'+data.data.children[j].data.permalink+'" target="_blank">'+data.data.children[j].data.title+'</a></div></div>');
+	  			$('#redditnews').append('<div class="row "><div class="col-lg-12 list-group-item list-group-item-action flex-column align-items-start" ><a href="https://reddit.com'+data.data.children[j].data.permalink+'" target="_blank">'+data.data.children[j].data.title+'</a></div></div>');
 		  		}else{
-					$('#redditnews').append('<div class="row "><div class="col-lg-12 bg-dark text-white " ><a href="https://reddit.com'+data.data.children[j].data.permalink+'" target="_blank">'+data.data.children[j].data.title+'</a></div></div>'); 		
+					$('#redditnews').append('<div class="row "><div class="col-lg-12 list-group-item list-group-item-action flex-column align-items-start" ><a href="https://reddit.com'+data.data.children[j].data.permalink+'" target="_blank">'+data.data.children[j].data.title+'</a></div></div>'); 		
 				}	
 	  		}
 	  		
 		});
+		$('#selectedCoin').append(name);
 	}
 	
 
