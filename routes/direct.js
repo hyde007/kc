b932774af63e217775d70b0990710678795596f8
Sharath <sballari@sapient.com>
2018-02-21 21:39:36 +0530

    SEO updates

--- routes/direct.js
+++ routes/direct.js
@@ -8,7 +8,7 @@ router.get('/coin/:id1/:id2',function(req,res){
   }else{
   	var domainName = 'http://localhost:8081';
   }
-  res.render('index',{domain:domainName,coin:req.params.id1,name:req.params.id2});
+  res.render('index',{domain:domainName,coin:req.params.id1,name:req.params.id2,homepage:false});
 });
 
 router.get('/',function(req,res){
@@ -17,7 +17,7 @@ router.get('/',function(req,res){
   }else{
   	var domainName = 'http://localhost:8081';
   }
-  res.render('index',{domain:domainName,coin:'BTC',name:'Bitcoin'});
+  res.render('index',{domain:domainName,coin:'BTC',name:'Bitcoin',homepage:true});
 });
 
 module.exports = router;
